c06795300453e556eb4981e86cc09792f61c87d2
Kariaki58 <programmerjavascript901@gmail.com>
2023-10-11 22:01:00 +0100

    mistakenly worked in main

--- models/base_model.py
+++ models/base_model.py
@@ -19,24 +19,24 @@ class BaseModel:
         args: any
         kwargs : to be used
         """
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
+        time_format = "%Y-%m-%dT%H:%M:%S.%f" 
+        if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(value, time_format)
                 else:
                     self.__dict__[key] = value
         else:
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
             models.storage.new(self)
 
     def __str__(self):
         """
         string representation of the objects
         """
-        return f"[{__class__.__name__}]({self.id}){self.__dict__}"
+        return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
@@ -51,8 +51,10 @@ class BaseModel:
         return a dictionary containing all keys/value
         of __dict__ instance
         """
-        d_instances = self.__dict__.copy()
+        d_instances = self.__dict__
+        if isinstance(self.created_at, datetime):
+            d_instances["created_at"] = self.created_at.isoformat()
+        if isinstance(self.updated_at, datetime):
+            d_instances["updated_at"] = self.updated_at.isoformat()
         d_instances["__class__"] = self.__class__.__name__
-        d_instances["created_at"] = self.created_at.isoformat()
-        d_instances["updated_at"] = self.updated_at.isoformat()
         return d_instances
